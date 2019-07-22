c3ee7721242b6934e5add4ed0b608b7519aaf971
Victor Nercio <vmanuelns@hotmail.com>
2019-07-22 15:34:24 -0400

    Prepping modal refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Notes from './modules/notes'
+import Modals from './modules/modals'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store ({
     modules: {
-        Notes
+        Notes,
+        Modals
     }
 })
