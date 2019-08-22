4f4832b8a0f6ba2c9fdc7b05e846a821b75506b3
Victor Nercio <vmanuelns@hotmail.com>
2019-08-22 19:19:44 -0400

    Fonts included to the client

--- src/main.js
+++ src/main.js
@@ -5,13 +5,18 @@ import store from './store'
 import NavBar from './components/elements/NavBar'
 import FooterBar from './components/elements/FooterBar'
 import NoteList from './components/elements/NoteList'
+import NoteModals from './components/modals/NoteModals'
 import SeeNoteModal from './components/modals/SeeNoteModal'
+import EditNoteModal from './components/modals/EditNoteModal'
 
 
 Vue.component('nav-bar', NavBar)
 Vue.component('footer-bar', FooterBar)
 Vue.component('note-list', NoteList)
+Vue.component('note-modals', NoteModals)
 Vue.component('see-note-modal', SeeNoteModal)
+Vue.component('edit-note-modal', EditNoteModal)
+
 
 new Vue({
   el: '#app',
