ec4452c36437cf27f0cb1c7eb58335b73df769d0
vmanun <vmanuelns@hotmail.com>
2019-09-24 12:57:59 -0400

    created modals state

--- src/store/modules/modals.js
+++ src/store/modules/modals.js
@@ -1,6 +1,37 @@
 export default {
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {}
-}
+  state: {
+    isOpen: false,
+    type: "",
+    note: {}
+  },
+  getters: {
+    getModalStatus: state => state.isOpen,
+    getType: state => state.type,
+    getNote: state => state.note
+  },
+  mutations: {
+    openModal(state, options = { type: "read", note: {} }) {
+      state.isOpen = true;
+      state.type = options.type;
+      state.note = options.note;
+    },
+    closeModal(state) {
+      state.isOpen = false;
+      state.type = "";
+    },
+    deleteModalNote: (state) => { state.note = {}; }
+  },
+  actions: {
+    closeModal({ commit }, options = { type: "", note: {} }) {
+      if (options) {
+        commit("closeModal");
+        setTimeout(() => {
+          commit("openModal", options);
+        }, 200);
+      } else {
+        commit("closeModal");
+        setTimeout(() => { commit('deleteModalNote'); }, 600);
+      }
+    }
+  }
+};
