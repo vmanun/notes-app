5cb413de61b3e1eaa23d682887cdcca2e5b83388
vmanun <vmanuelns@hotmail.com>
2019-09-24 12:56:00 -0400

    Added NoteModals proxy component for note modals

---
+++ src/components/modals/NoteModals.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="note-modals">
+    <see-note-modal :show="getModalStatus && getType === 'read'" :note="getNote"></see-note-modal>
+    <edit-note-modal :show="getModalStatus && getType === 'edit' && true" :note="getNote"></edit-note-modal>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: { ...mapGetters(['getModalStatus', 'getType', 'getNote']) }, 
+}
+</script>
+<style>
+  /*Modal rules*/
+@import url('../../styles/modal.css');
+</style>
+
