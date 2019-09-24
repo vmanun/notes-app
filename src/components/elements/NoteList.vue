28d460eb07d37e55d2e2af3f1cd4f937981975b1
vmanun <vmanuelns@hotmail.com>
2019-09-24 13:00:01 -0400

    Addapted to the created modal state

--- src/components/elements/NoteList.vue
+++ src/components/elements/NoteList.vue
@@ -14,70 +14,22 @@
         </div>
       </div>
     </div>
-    <see-note-modal :show="noteIsOpened" :note="noteOpened" @onclose="openNote"></see-note-modal>
+    <note-modals></note-modals>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 export default {
-  data() {
-    return{
-      noteIsOpened: false,
-      noteOpened: {}
-    }
-  },
   methods: {
+    ...mapMutations(['openModal']),
     openNote(note) {
-      this.noteIsOpened = !this.noteIsOpened
-      this.noteOpened = note 
-    }
+      this.openModal({ type: 'read', note })
+    },
   },
   computed: mapGetters(['getNotes']),
 };
 </script>
 
-
 <style>
-/*Note setup*/
-@import url('../../styles/NoteGeneral.css');
-
-/*notes container and note elements*/
-.note-container {
-  -webkit-display: flex;
-  -moz-display: flex;
-  -ms-display: flex;
-  display: flex;
-  -webkit-flex-direction: row;
-  -moz-flex-direction: row;
-  -ms-flex-direction: row;
-  -webkit-flex-wrap: wrap;
-  -moz-flex-wrap: wrap;
-  -ms-flex-wrap: wrap;
-  -webkit-justify-content: space-around;
-  -moz-justify-content: space-around;
-  -ms-justify-content: space-around;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  margin-left: 35px;
-}
-
-.note {
-  -webkit-box-shadow: 5px 5px 15px #666, -5px -5px 15px #666;
-  -moz-box-shadow: 5px 5px 15px #666, -5px -5px 15px #666;
-  box-shadow: 5px 5px 15px #666, -5px -5px 15px #666;
-  height: 265px;
-  max-width: 300px;
-  margin: 10px;
-  -webkit-flex-basis: 300px;
-  -moz-flex-basis: 300px;
-  -ms-flex-basis: 300px;
-  flex-basis: 300px;
-  -webkit-flex-grow: 2;
-  -moz-flex-grow: 2;
-  -ms-flex-grow: 2;
-  flex-grow: 2;
-  cursor: pointer;
-}
 </style>
